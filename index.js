cc931f2fd943129c48844d92baf0093fc8a53c6d
InnocentEdem <innocent.kuwornu@amalitech.org>
2022-02-28 12:23:30 +0000

    set headers with helmet

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require("dotenv").config()
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const express = require('express');
 const morgan = require('morgan');
-const { sequelize,user,post,refresh_token } = require('./models/index');
+const { sequelize } = require('./models/index');
 const routes = require('./routes')
 const app = express();
-const jwt = require("jsonwebtoken");
-const res = require("express/lib/response");
 const allowedMethods = require('./utils/allowedMethods')
 const PORT = 5001
 
+app.use(express.json())
+app.use(helmet()) // set security headers
 app.use(cookieParser())
 app.use(morgan('tiny'))
-app.use(express.json())
 app.use(bodyParser.urlencoded({extended :true}));
 app.use(bodyParser.json())
 app.use("/",allowedMethods, routes)
